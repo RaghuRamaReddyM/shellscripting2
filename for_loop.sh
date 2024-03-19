e35a9ab7f6e420af141b98569d5e6e3046aa30c1
RaghuRamaReddyM <158450441+RaghuRamaReddyM@users.noreply.github.com>
2024-03-19 16:37:27 +0530

    Update for_loop.sh

--- for_loop.sh
+++ for_loop.sh
@@ -15,3 +15,34 @@ do
         echo $i
         sleep 2
 done
+#!/bin/bash
+
+
+#odd number#
+for i in {1..10..2}
+do
+        echo $i
+        sleep 2
+done
+
+#even number#
+#!/bin/bash
+
+for i in {0..10..2}
+do
+        echo $i
+        sleep 2
+done
+
+#10th table#
+#!/bin/bash
+
+number=10
+for i in {1..10}
+do
+        echo "$number * $i =$((number * i))"
+
+done
+
+
+
